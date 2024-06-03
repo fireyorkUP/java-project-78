c8a273defaddef63159f12251efd26cfddc404e8
fireyork <andrre2001@gmail.com>
2024-06-03 14:48:22 +0300

    fix hexlet checkstyle

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -6,13 +6,13 @@ import java.util.function.Predicate;
 
 public abstract class BaseSchema<T> {
     protected boolean isRequired = false;
-    protected Map<String, Predicate<T>> checks = new LinkedHashMap<>();
+    protected Map<String, Predicate<Object>> checks = new LinkedHashMap<>();
 
     /**
      * @param checkName allows you to add check name to map
      * @param predicate allows you to add predicates to map
      */
-    protected final void addCheck(String checkName, Predicate<T> predicate) {
+    protected final void addCheck(String checkName, Predicate<Object> predicate) {
         checks.put(checkName, predicate);
     }
 
@@ -25,7 +25,7 @@ public abstract class BaseSchema<T> {
      * @param obj Strongly parametrized object
      * @return true if all validations passed, false otherwise
      */
-    protected boolean isValid(T obj) {
+    public final boolean isValid(Object obj) {
         if (obj == null || obj.equals("")) {
             return !isRequired;
         }
